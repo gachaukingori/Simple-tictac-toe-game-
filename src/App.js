a89428a1242e2ac937bcdc4f32272473445b1b71
gachaukingori <victor.kingori93@gmail.com>
2022-01-06 23:39:02 +0300

    simple tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+class ShoppingList extends  React.Component {
+    render(){
+        return(
+            <div className="shopping-list"> 
+                <ul> 
+                    <li>Whatsapp </li>
+                    <li>Facebook </li>
+                    <li>Instagram </li>
+                </ul>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            </div>
+        );
+
+    }
+
 }
-
-export default App;
